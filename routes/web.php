4b19bea800c8f3d224b486620d70cce5da7a9173
btoz <btozapi@api.btoz.co.id>
2019-03-25 13:17:41 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,24 @@ Auth::routes();
 Route::get('/dashboard', 'CMSController@dashboard');#->name('home');
 
 Route::resource('usermgmt', 'CMS\UsermgmtController');
+Route::post('usermgmtPostRoleDropdown', 'CMS\UsermgmtController@postRoleDropdown')->name('usermgmt.postRoleDropdown');
 
-Route::resource('report', 'CMS\ReportController');
+// Route::resource('transReport', 'CMS\TransReportController');
+Route::get('transReport', 'CMS\TransReportController@index')->name('transReport.index');
 
-Route::get('/reports/{dateStart?}/{dateEnd?}', 'CMSController@reports')
-	->where([
-		'dateStart' => '^\d{1,2}\-\d{1,2}\-\d{4}$',
-		'dateEnd' => '^\d{1,2}\-\d{1,2}\-\d{4}$'
-	]);
+// Route::resource('rkmReport', 'CMS\RKMReportController');
+Route::get('rkmReport', 'CMS\RKMReportController@index')->name('rkmReport.index');
 
-Route::resource('article', 'CMSController');
+// Route::resource('customReport', 'CMS\CustomReportController');
+Route::get('customReport', 'CMS\CustomReportController@index')->name('customReport.index');
+Route::post('postDropdown', 'CMS\CustomReportController@postDropdown')->name('postDropdown');
+Route::post('postFilter', 'CMS\CustomReportController@postFilter')->name('postFilter');
+
+// Route::get('/reports/{dateStart?}/{dateEnd?}', 'CMSController@reports')
+// 	->where([
+// 		'dateStart' => '^\d{1,2}\-\d{1,2}\-\d{4}$',
+// 		'dateEnd' => '^\d{1,2}\-\d{1,2}\-\d{4}$'
+// 	]);
+
+// Route::resource('article', 'CMSController');
 
