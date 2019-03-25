ff387b60cacfc79c955f8a38aa150db2804e7511
btoz <btozapi@api.btoz.co.id>
2019-03-25 13:22:03 +0700

    Create changeDBconnection.php
    middleware for changing connection

---
+++ app/Http/Middleware/changeDBconnection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+// use DB;
+use Config;
+
+class changeDBconnection
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Config::set('database.default', 'sqlsrv2');
+        // DB::connection('sqlsrv2');
+        return $next($request);
+    }
+}
