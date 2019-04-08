7253c3ce94fd8ee8d1cef5d58b0708403e8c22c8
btoz <btozapi@api.btoz.co.id>
2019-04-08 02:08:11 +0000

    pembagian transaksi report menjadi bagian mandor dan kawil

--- app/Http/Controllers/CMS/TransReportController.php
+++ app/Http/Controllers/CMS/TransReportController.php
@@ -10,28 +10,47 @@ use App\Http\Controllers\CMSController;
 class TransReportController extends CMSController
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the mandor resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function indexMandor(Request $request)
     {
         if ($request->ajax()) {
-            $query = DB::table('EWS_TRANS_MANDOR')
-            ->join('users', 'users.id', '=', 'EWS_TRANS_MANDOR.userid')
-            ->join('EWS_MANDOR', 'EWS_MANDOR.codePekerja', '=', 'users.codePekerja')
-            ->join('EWS_PEKERJA as m', 'm.codePekerja', '=', 'users.codePekerja')
-            ->join('EWS_PEKERJA as t', 't.codePekerja', '=', 'EWS_TRANS_MANDOR.codeTukang')
-            ->join('EWS_SUB_JOB', 'EWS_SUB_JOB.subJobCode', '=', 'EWS_TRANS_MANDOR.subJobCode')
-            ->select('EWS_TRANS_MANDOR.*', 'users.codePekerja as codePekerjaMandor', 'EWS_MANDOR.codeMandor', 'm.namaPekerja as namaMandor', 't.namaPekerja as namaTukang', 'EWS_SUB_JOB.Description')
-            ->selectRaw('convert(varchar, EWS_TRANS_MANDOR.created_at, 106) as tanggal')
-            ->selectRaw('convert(varchar, EWS_TRANS_MANDOR.created_at, 8) as waktu')
+            $query = DB::table('EWS_VW_CMS_MANDOR_TRANS')
+            ->get();
+            // $query = DB::table('EWS_TRANS_MANDOR')
+            // ->join('users', 'users.id', '=', 'EWS_TRANS_MANDOR.userid')
+            // ->join('EWS_MANDOR', 'EWS_MANDOR.codePekerja', '=', 'users.codePekerja')
+            // ->join('EWS_PEKERJA as m', 'm.codePekerja', '=', 'users.codePekerja')
+            // ->join('EWS_PEKERJA as t', 't.codePekerja', '=', 'EWS_TRANS_MANDOR.codeTukang')
+            // ->join('EWS_SUB_JOB', 'EWS_SUB_JOB.subJobCode', '=', 'EWS_TRANS_MANDOR.subJobCode')
+            // ->select('EWS_TRANS_MANDOR.*', 'users.codePekerja as codePekerjaMandor', 'EWS_MANDOR.codeMandor', 'm.namaPekerja as namaMandor', 't.namaPekerja as namaTukang', 'EWS_SUB_JOB.Description')
+            // ->selectRaw('convert(varchar, EWS_TRANS_MANDOR.created_at, 106) as tanggal')
+            // ->selectRaw('convert(varchar, EWS_TRANS_MANDOR.created_at, 8) as waktu')
+            // ->get();
+            $report = $this->removeWhitespace($query);
+            return DataTables::of($report)
+            ->make(true);
+        }
+        return view('cms.mandorTransReport');
+    }
+
+    /**
+     * Display a listing of the kawil resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function indexKawil(Request $request)
+    {
+        if ($request->ajax()) {
+            $query = DB::table('EWS_VW_CMS_KAWIL_TRANS')
             ->get();
             $report = $this->removeWhitespace($query);
             return DataTables::of($report)
             ->make(true);
         }
-        return view('cms.transReport');
+        return view('cms.kawilTransReport');
     }
 
     /**
