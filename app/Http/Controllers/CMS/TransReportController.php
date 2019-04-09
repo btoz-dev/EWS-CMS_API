d3281b24e04fa061ca54448e9646221e736e4aa2
btoz <btozapi@api.btoz.co.id>
2019-04-09 00:44:02 +0000

    penambahan whereBetween rkhDate di dalam fungsi

--- app/Http/Controllers/CMS/TransReportController.php
+++ app/Http/Controllers/CMS/TransReportController.php
@@ -17,22 +17,21 @@ class TransReportController extends CMSController
     public function indexMandor(Request $request)
     {
         if ($request->ajax()) {
-            $query = DB::table('EWS_VW_CMS_MANDOR_TRANS')
-            ->get();
-            // $query = DB::table('EWS_TRANS_MANDOR')
-            // ->join('users', 'users.id', '=', 'EWS_TRANS_MANDOR.userid')
-            // ->join('EWS_MANDOR', 'EWS_MANDOR.codePekerja', '=', 'users.codePekerja')
-            // ->join('EWS_PEKERJA as m', 'm.codePekerja', '=', 'users.codePekerja')
-            // ->join('EWS_PEKERJA as t', 't.codePekerja', '=', 'EWS_TRANS_MANDOR.codeTukang')
-            // ->join('EWS_SUB_JOB', 'EWS_SUB_JOB.subJobCode', '=', 'EWS_TRANS_MANDOR.subJobCode')
-            // ->select('EWS_TRANS_MANDOR.*', 'users.codePekerja as codePekerjaMandor', 'EWS_MANDOR.codeMandor', 'm.namaPekerja as namaMandor', 't.namaPekerja as namaTukang', 'EWS_SUB_JOB.Description')
-            // ->selectRaw('convert(varchar, EWS_TRANS_MANDOR.created_at, 106) as tanggal')
-            // ->selectRaw('convert(varchar, EWS_TRANS_MANDOR.created_at, 8) as waktu')
-            // ->get();
-            $report = $this->removeWhitespace($query);
+            $query = DB::table('EWS_VW_CMS_MANDOR_TRANS');
+            
+            if ($request->date_aw != NULL) {
+                # code...
+                $query->whereBetween('created_at', [$request->date_aw, $request->date_ak." 23:59:59.000"]);
+            }
+
+            $res = $query->get();
+
+            $report = $this->removeWhitespace($res);
+
             return DataTables::of($report)
-            ->make(true);
+                ->make(true);
         }
+
         return view('cms.mandorTransReport');
     }
 
@@ -44,12 +43,21 @@ class TransReportController extends CMSController
     public function indexKawil(Request $request)
     {
         if ($request->ajax()) {
-            $query = DB::table('EWS_VW_CMS_KAWIL_TRANS')
-            ->get();
-            $report = $this->removeWhitespace($query);
+            $query = DB::table('EWS_VW_CMS_KAWIL_TRANS');
+
+            if ($request->date_aw != NULL) {
+                # code...
+                $query->whereBetween('created_at', [$request->date_aw, $request->date_ak." 23:59:59.000"]);
+            }
+
+            $res = $query->get();
+
+            $report = $this->removeWhitespace($res);
+
             return DataTables::of($report)
-            ->make(true);
+                ->make(true);
         }
+        
         return view('cms.kawilTransReport');
     }
 
