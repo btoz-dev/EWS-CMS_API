c6560d79819b096293f40af888baa25c989df8bc
btoz <btozapi@api.btoz.co.id>
2019-04-22 07:59:46 +0000

    new model trans model and kawil

---
+++ app/Trans.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class Trans extends Model
+{
+    //
+    public static function mandor($job)
+    {
+    	# code...
+    	$query = DB::table('EWS_VW_CMS_MANDOR_TRANS')
+	                ->select('*')
+	                ->selectRaw('convert(CHAR(17), created_at, 113) as created_at');
+
+    	if ($job == 'PLANTCARE') {
+    		# code...
+    		$query->where('jobCode', '=', '001');
+    	}
+
+    	if ($job == 'FRUITCARE') {
+    		# code...
+    		$query->where('jobCode', '=', '002');
+    	}
+
+    	return $query;
+    }
+
+    public static function kawil($job)
+    {
+    	# code...
+    	$query = DB::table('EWS_VW_CMS_KAWIL_TRANS')
+	                ->select('*')
+	                ->selectRaw('convert(CHAR(17), created_at, 113) as created_at');
+
+    	if ($job == 'PLANTCARE') {
+    		# code...
+    		$query->where('jobCode', '=', '001');
+    	}
+
+    	if ($job == 'FRUITCARE') {
+    		# code...
+    		$query->where('jobCode', '=', '002');
+    	}
+
+    	return $query;
+    }
+}
