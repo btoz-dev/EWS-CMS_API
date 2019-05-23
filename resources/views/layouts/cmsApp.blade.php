e88e4afe87ab47c43544b1b79bfe9b3b81e39d84
btoz <btozapi@api.btoz.co.id>
2019-05-23 21:08:57 +0700

    use local assets | implement permission to each menu

--- resources/views/layouts/cmsApp.blade.php
+++ resources/views/layouts/cmsApp.blade.php
@@ -11,14 +11,14 @@
     <title>{{ config('app.name', 'EWS') }} CMS</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}">
 
 
     <!-- Dashboard CSS -->
     <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
     
     <!-- DataTables CSS -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/af-2.3.3/b-1.5.6/b-colvis-1.5.6/b-flash-1.5.6/b-html5-1.5.6/b-print-1.5.6/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-2.0.0/sl-1.3.0/datatables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/datatables.min.css') }}"/>
     @yield('stylesheet')
 
 </head>
@@ -52,70 +52,125 @@
                             Dashboard <span class="sr-only">(current)</span>
                             </a>
                         </li>
+                        @can('view_usermgmt')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('usermgmt*') ? 'active' : '' }}" href="{{ url('/usermgmt') }}">
+                                <i class="fas fa-users"></i>
+                                User Management
+                                </a>
+                            </li>
+                        @endcan
+                        @can('view_roles')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('roles*') ? 'active' : '' }}" href="{{ url('/roles') }}">
+                                <i class="fas fa-users"></i>
+                                Role Management
+                                </a>
+                            </li>
+                        @endcan
+                        @can('view_rkmReport')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('rkmReport*') ? 'active' : '' }}" href="{{ url('/rkmReport') }}">
+                                <i class="far fa-chart-bar"></i>
+                                RKM Reports
+                                </a>
+                            </li>
+                        @endcan
+                        @can('view_spiPlantReport')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('mandorPlantcareReport*') ? 'active' : '' }}" href="{{ url('/mandorPlantcareReport') }}">
+                                <i class="far fa-chart-bar"></i>
+                                SPI Plantcare Reports
+                                </a>
+                            </li>
+                        @endcan
+                        @can('view_mandorFruitReport')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('mandorFruitcareReport*') ? 'active' : '' }}" href="{{ url('/mandorFruitcareReport') }}">
+                                <i class="far fa-chart-bar"></i>
+                                Mandor Fruitcare Reports
+                                </a>
+                            </li>
+                        @endcan
+                        @can('view_mandorPanenReport')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('mandorPanenReport*') ? 'active' : '' }}" href="{{ url('/mandorPanenReport') }}">
+                                <i class="far fa-chart-bar"></i>
+                                Mandor Panen Reports
+                                </a>
+                            </li>
+                        @endcan
+                        @can('view_kawilPlantReport')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('kawilPlantcareReport*') ? 'active' : '' }}" href="{{ url('/kawilPlantcareReport') }}">
+                                <i class="far fa-chart-bar"></i>
+                                Kawil Plantcare Reports
+                                </a>
+                            </li>
+                        @endcan
+                        @can('view_kawilFruitReport')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('kawilFruitcareReport*') ? 'active' : '' }}" href="{{ url('/kawilFruitcareReport') }}">
+                                <i class="far fa-chart-bar"></i>
+                                Kawil Fruitcare Reports
+                                </a>
+                            </li>
+                        @endcan
+                        @can('view_kawilPanenReport')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('kawilPanenReport*') ? 'active' : '' }}" href="{{ url('/kawilPanenReport') }}">
+                                <i class="far fa-chart-bar"></i>
+                                Kawil Panen Reports
+                                </a>
+                            </li>
+                        @endcan
                         <li class="nav-item">
-                            <a class="nav-link {{ request()->is('usermgmt*') ? 'active' : '' }}" href="{{ url('/usermgmt') }}">
-                            <i class="fas fa-users"></i>
-                            User Management
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ request()->is('rkmReport*') ? 'active' : '' }}" href="{{ url('/rkmReport') }}">
+                            <a class="nav-link {{ request()->is('phbtReport*') ? 'active' : '' }}" href="{{ url('/phbtReport') }}">
                             <i class="far fa-chart-bar"></i>
-                            RKM Reports
+                            PH BT Reports
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link {{ request()->is('mandorPlantcareReport*') ? 'active' : '' }}" href="{{ url('/mandorPlantcareReport') }}">
+                            <a class="nav-link {{ request()->is('phhtReport*') ? 'active' : '' }}" href="{{ url('/phhtReport') }}">
                             <i class="far fa-chart-bar"></i>
-                            SPI Plantcare Reports
+                            PH HT Reports
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link {{ request()->is('mandorFruitcareReport*') ? 'active' : '' }}" href="{{ url('/mandorFruitcareReport') }}">
+                            <a class="nav-link {{ request()->is('phcltReport*') ? 'active' : '' }}" href="{{ url('/phcltReport') }}">
                             <i class="far fa-chart-bar"></i>
-                            Mandor Fruitcare Reports
+                            PH CLT Reports
                             </a>
                         </li>
+                        @can('view_customReport')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('customReport*') ? 'active' : '' }}" href="{{ url('/customReport') }}">
+                                <i class="far fa-chart-bar"></i>
+                                Custom Reports
+                                </a>
+                            </li>
+                        @endcan
+                        @can('view_apk')
+                            <li class="nav-item">
+                                <a class="nav-link {{ request()->is('apk*') ? 'active' : '' }}" href="{{ url('/apk') }}">
+                                <i class="fab fa-android"></i>
+                                APK
+                                </a>
+                            </li>
+                        @endcan
                         <li class="nav-item">
-                            <a class="nav-link {{ request()->is('mandorPanenReport*') ? 'active' : '' }}" href="{{ url('/mandorPanenReport') }}">
-                            <i class="far fa-chart-bar"></i>
-                            Mandor Panen Reports
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ request()->is('kawilPlantcareReport*') ? 'active' : '' }}" href="{{ url('/kawilPlantcareReport') }}">
-                            <i class="far fa-chart-bar"></i>
-                            Kawil Plantcare Reports
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ request()->is('kawilFruitcareReport*') ? 'active' : '' }}" href="{{ url('/kawilFruitcareReport') }}">
-                            <i class="far fa-chart-bar"></i>
-                            Kawil Fruitcare Reports
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ request()->is('kawilPanenReport*') ? 'active' : '' }}" href="{{ url('/kawilPanenReport') }}">
-                            <i class="far fa-chart-bar"></i>
-                            Kawil Panen Reports
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ request()->is('customReport*') ? 'active' : '' }}" href="{{ url('/customReport') }}">
-                            <i class="far fa-chart-bar"></i>
-                            Custom Reports
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ request()->is('apk*') ? 'active' : '' }}" href="{{ url('/apk') }}">
+                            <a class="nav-link {{ request()->is('users*') ? 'active' : '' }}" href="{{ url('/users') }}">
                             <i class="fab fa-android"></i>
-                            APK
+                            user admin test
                             </a>
                         </li>
                     </ul>
                 </div>
             </nav>
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+                <div id="flash-msg">
+                    @include('flash::message')
+                </div>
                 @yield('content')
             </main>
         </div>
@@ -125,18 +180,18 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <!-- jQuery -->
     <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
     <!-- Popper -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
     <!-- Bootstrap -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     
     <!-- DataTable -->
     <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script> -->
     <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script> -->
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/af-2.3.3/b-1.5.6/b-colvis-1.5.6/b-flash-1.5.6/b-html5-1.5.6/b-print-1.5.6/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-2.0.0/sl-1.3.0/datatables.min.js"></script>
+    <script type="text/javascript" src="{{ asset('js/datatables.min.js') }}"></script>
 
-    <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
+    <script defer src="{{ asset('js/all.js') }}"></script>
     @yield('script')
     <script>
       var msg = '{{Session::get('alert')}}';
