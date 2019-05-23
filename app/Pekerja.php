d549056fcd142af5c1c967ff45a5ef267ff24586
btoz <btozapi@api.btoz.co.id>
2019-05-23 20:52:43 +0700

    add PK | new func accessor 'getNameCodeAttribute'

--- app/Pekerja.php
+++ app/Pekerja.php
@@ -6,12 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pekerja extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
 	protected $table = 'dbo.EWS_PEKERJA';
+
+	/**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'codePekerja';
 
 	public function user()
 	{
 		# code...
 		return $this->belongsTo('App\User', 'codePekerja', 'codePekerja');
+	}
+
+	public function getNameCodeAttribute()
+	{
+		return "{$this->namaPekerja} [{$this->codePekerja}]";
 	}
 }
