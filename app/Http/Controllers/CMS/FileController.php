ccbaa16f05c30d8c757bfb40f6836ceb89ecdf6e
btoz <btozapi@api.btoz.co.id>
2019-05-24 10:51:05 +0700

    Update FileController.php
    add upload and download apk function for RKH and PH

--- app/Http/Controllers/CMS/FileController.php
+++ app/Http/Controllers/CMS/FileController.php
@@ -11,32 +11,54 @@ class FileController extends CMSController
     //
     public function index()
     {
-        $fileExist = Storage::disk('public')->exists('apk.log');
+        $fileRKHExist = Storage::disk('public')->exists('apk_rkh.log');
+        $filePHExist = Storage::disk('public')->exists('apk_ph.log');
         // return var_dump($fileExist);
-        if (!$fileExist)
+        if (!$fileRKHExist)
         {
-            Storage::put('public/apk.log', 'log apk files');
+            Storage::put('public/apk_rkh.log', 'log apk files');
+        }
+        if (!$filePHExist)
+        {
+            Storage::put('public/apk_ph.log', 'log apk files');
         }
         // Storage::put('public/apk.log', 'log apk files');
 
-        $data['logTxt'] = Storage::get('public/apk.log');
+        $data['logTxtRKH'] = Storage::get('public/apk_rkh.log');
+        $data['logTxtPH'] = Storage::get('public/apk_ph.log');
 
         return view('cms.insertAPK', $data);
     }
 
-    public function upload(Request $request)
+    public function uploadApkRKH(Request $request)
     {
         # code...
         // return $request;
-        $path = $request->file('file')->storeAs('/public/apkfile', 'ews.apk');
+        $path = $request->file('file')->storeAs('/public/apkfile', 'ews_rkh.apk');
 
-        Storage::put('public/apk.log', $request->logapk);
+        Storage::put('public/apk_rkh.log', $request->logapk);
 
         if (!empty($path))
         {
-            return redirect()->back()->with('alert', 'File has been uploaded');
+            return redirect()->back()->with('alertRKH', 'File has been uploaded');
         }else{
-            return redirect()->back()->with('alert', 'Failed to upload. '.$path);
+            return redirect()->back()->with('alertRKH', 'Failed to upload. '.$path);
+        }
+    }
+
+    public function uploadApkPH(Request $request)
+    {
+        # code...
+        // return $request;
+        $path = $request->file('file')->storeAs('/public/apkfile', 'ews_ph.apk');
+
+        Storage::put('public/apk_ph.log', $request->logapk);
+
+        if (!empty($path))
+        {
+            return redirect()->back()->with('alertPH', 'File has been uploaded');
+        }else{
+            return redirect()->back()->with('alertPH', 'Failed to upload. '.$path);
         }
     }
 }
