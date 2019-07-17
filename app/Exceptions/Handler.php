7194455c8a2efe04c2f98238bbaf9900ae49aeb7
btoz <btozapi@api.btoz.co.id>
2019-07-17 15:50:52 +0700

    routing unauthorized user to home

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\Access\AuthorizationException as AuthorizationException;
 
 class Handler extends ExceptionHandler
 {
