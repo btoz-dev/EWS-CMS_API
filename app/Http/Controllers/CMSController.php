d1ee5397d364a7755240eefaf495add0ba1e52fb
btoz <btozapi@api.btoz.co.id>
2019-07-24 14:03:43 +0700

    max execc time and memory limit

--- app/Http/Controllers/CMSController.php
+++ app/Http/Controllers/CMSController.php
@@ -17,6 +17,11 @@ class CMSController extends Controller
     public function __construct(Request $request)
     {
         $this->middleware('auth');
+        # code...
+        ini_set('max_execution_time', '3600');
+        // ini_set('max_execution_time', '1');
+        ini_set('memory_limit', '1G');
+        // ini_set('memory_limit', '100M');
     }
 
     /**
